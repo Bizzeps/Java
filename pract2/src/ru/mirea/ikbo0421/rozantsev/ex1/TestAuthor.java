284f65f80d9d3257b664727b9dcd17e56bdfc2c0
Bizzeps <90606376+Bizzeps@users.noreply.github.com>
2022-12-22 13:13:26 +0300

    Add files via upload

---
+++ pract2/src/ru/mirea/ikbo0421/rozantsev/ex1/TestAuthor.java
@@ -0,0 +1,8 @@
+package ru.mirea.ikbo0421.rozantsev.ex1;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a = new Author("Иван", "ivan@rozantsev.ru", 'm');
+        System.out.println(a);
+    }
+}
---
+++ pract3/src/ru/mirea/ikbo0421/rozantsev/exs45/Movable.java
@@ -0,0 +1,8 @@
+package ru.mirea.ikbo0421.rozantsev.exs45;
+
+public interface Movable {
+    void moveUp();
+    void moveDown();
+    void moveLeft();
+    void moveRight();
+}
